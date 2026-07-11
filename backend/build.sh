ba4cf05270f945bb144a0ada94d9eb2c896c5ad6
Mateo Guerrero <david.guerreroi@>
2026-07-11 18:57:31 -0500

    Add DB seeding and role setup to build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,8 @@ python manage.py collectstatic --no-input
 
 # Ejecutar las migraciones en la base de datos PostgreSQL de Render
 python manage.py migrate
+
+# Configurar roles y crear usuarios iniciales en la base de datos de producción
+python manage.py setup_avicola_roles
+python manage.py create_demo_users
+python manage.py ensure_admin_user
